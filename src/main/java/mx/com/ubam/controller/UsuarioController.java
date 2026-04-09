0734831c7b6e85dd6191b0571987503b80db44f8
Skelett0101 <critof0864@gmail.com>
2026-04-09 15:42:29 -0600

    Revert "4. Frontend"
    This reverts commit 27f38aca46136b992d0cd93302e3c6243feffa49.

--- src/main/java/mx/com/ubam/controller/UsuarioController.java
+++ src/main/java/mx/com/ubam/controller/UsuarioController.java
@@ -2,11 +2,9 @@ package mx.com.ubam.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import mx.com.ubam.model.Usuario;
 import mx.com.ubam.service.UsuarioService;
-import mx.com.ubam.repository.UsuarioRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @RestController 
 @RequestMapping("/api/usuarios") 
@@ -16,26 +14,11 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-
-    // R01: Registrar usuario (Solo Admin - Protegido por SecurityConfig)
+    // R01: Registrar usuario
     @PostMapping("/registro")
     public Usuario registrar(@RequestBody Usuario usuario) {
-    	return usuarioService.registrarUsuario(usuario);
-    }
-    // R04: Listar todos (Solo Admin)
-    @GetMapping
-    public List<Usuario> listarTodos() {
-        return usuarioRepository.findAll();
-    }
 
-    // R07: Ver perfil propio (Cualquier usuario logueado)
-    @GetMapping("/perfil")
-    public Usuario verPerfil(HttpServletRequest request) {
-        // Extrae el ID que el JwtFilter puso en el request al validar el token
-        Integer idUsuario = (Integer) request.getAttribute("usuarioId");
-        return usuarioRepository.findById(idUsuario)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        //   RequestBody convierte JSON a objeto Usuario
+        return usuarioService.registrarUsuario(usuario);
     }
 }
